393bb6e5339a02283e8b19c3d472b20bd72865a9
gbox3d <gbox3d@gmail.com>
2021-08-12 05:02:15 +0000

    ....

--- day4/exam03_rest_server/index.js
+++ day4/exam03_rest_server/index.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import { url } from 'inspector';
 import {URL} from 'url'
 
 const port = 8090
@@ -10,18 +11,30 @@ http.createServer((req,res)=> {
         'http://example.com'
     );
 
+    console.log(`pathname : ${urlObj.pathname}`);
+    console.log(`search : ${urlObj.search}`);
+    console.log(`hostname : ${urlObj.hostname}`);
+
     let result = {
         r: 'ok'
     }
-    // let header = {
-    //     'Content-Type': 'text/json',
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods': 'GET',
-    //     'Access-Control-Max-Age': '1000',
-    //     "Access-Control-Allow-Headers": "*" //CORS 정책 허용  * 는 모두 허용 
-    // }
-    // res.writeHead(header);
+    let header = {
+        'Content-Type': 'text/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Max-Age': '1000',
+        "Access-Control-Allow-Headers": "*" //CORS 정책 허용  * 는 모두 허용 
+    }
+    res.writeHead(200,header);
+
+    let _title = urlObj.searchParams.get('title');
+    let _msg = urlObj.searchParams.get('msg');
+
+    result.title = _title
+    result.msg = _msg
+
     res.end(JSON.stringify(result));
+
 }).listen(port);
 
 console.log(`listen : ${port}`);

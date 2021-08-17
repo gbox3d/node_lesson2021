4fdb222eba63b4c5073d5e4b5d3257f9546f7846
gbox3d <gbox3d@gmail.com>
2021-08-17 14:38:30 +0900

    ....

--- day6/exam02_mw/index.js
+++ day6/exam02_mw/index.js
@@ -1,36 +1,43 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import { logger, autholize, testmw_1, testmw_2 } from './mw.js'
 dotenv.config()
 
 const app = express()
 
-app.use((req,res,next)=> {
-    console.log(req.method,req.url,(new Date()).toISOString())
-    next()
-})
+// app.use(logger)
+// app.use(autholize) //전역 스코프 
+app.use(
+    [morgan(':method :url :status :res[content-length] - :response-time ms'),
+autholize]) //배열로 다수 넘기기 
 
-app.use((req,res,next)=> {
-    if(req.query.apikey == 'qwer') {
-        next()
-    }
-    else {
-        res.send('<h1>인증이 필요합니다.</h1>')
-    }
-})
+app.use('/home', [testmw_1,morgan(':remote-addr')]) // home 스코프
+app.use('/about', testmw_2) // about 스코프 
 
-
-app.get('/home',(req,res)=> {
+app.get('/home', (req, res) => {
 
     res.send('<h1>home</h1>')
 
 })
+app.get('/home/welcome', (req, res) => {
+    res.send('<h1>welcome to home </h1>')
+})
+app.get('/about', (req, res) => {
 
-app.get('/about',(req,res)=> {
-    
     res.send('<h1>about</h1>')
 })
 
+//use 쓰지 않고 직접 미들웨어 등록하기 
+app.get('/blog',
+    [(req, res, next) => { //미들웨어 임배딩 
+        console.log('blog middle ware')
+        next()
+    }],
+    (req, res) => { //콜백 
+        res.send('<h1> blog </h1>')
+    })
 
-app.listen(process.env.PORT,()=> {
+app.listen(process.env.PORT, () => {
     console.log(`server start at : ${process.env.PORT}`)
 })

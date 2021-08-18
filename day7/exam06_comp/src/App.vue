8349cbb64d33dfb175a7e337d82a50a1bcf3dd79
gbox3d <gbox3d@gmail.com>
2021-08-18 15:54:46 +0900

    ....

---
+++ day7/exam06_comp/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TextBox />
+  <TextBox />
+  <TextBox />
+  <TextBox />
+  <TextBox />
+  
+</template>
+
+<script>
+
+import TextBox from './components/TextBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TextBox
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ day7/exam06_comp/src/components/TextBox.vue
@@ -0,0 +1,33 @@
+<template >
+
+<div>
+  <h1 class="title">Title</h1>
+  <p> 내용 </p>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name : "TextBox",
+    data() {
+        return {
+
+        }
+    },
+    methods:{
+
+    },
+    created() {
+
+    }
+}
+</script>
+
+<style scoped>
+div {
+  border: solid black 1px;
+  background-color: gainsboro;
+}
+    
+</style>

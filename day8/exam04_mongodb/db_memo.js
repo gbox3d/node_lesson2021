333fea082f185a25ef3f3fc894db479ec96ce0ce
gbox3d <gbox3d@gmail.com>
2021-08-19 17:35:17 +0900

    ...

---
+++ day8/exam04_mongodb/db_memo.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import {ObjectId} from 'mongodb'
+
+const router = express.Router()
+
+export default (db_client) => {
+    router.post('/insert', async (req, res) => {
+
+        try {
+            const database = db_client.db('node_stduy');
+            const memo = database.collection('memo');
+
+            console.log(req.body)
+
+            let _res = await memo.insertOne(req.body);
+
+            res.json({ r: 'ok', d: _res });
+        }
+        catch (e) {
+            res.json({ r: 'err', err: e })
+        }
+    });
+
+    router.get('/find/skip/:skip/limit/:limit', async (req, res) => {
+        try {
+            console.log(req.params)
+            const database = db_client.db('node_stduy');
+            const memo = database.collection('memo');
+            let cursor = await memo.find()
+                .skip(parseInt(req.params.skip))
+                .limit(parseInt(req.params.limit));
+
+            let items = await cursor.toArray();
+            res.json({ r: 'ok', d: items });
+        }
+        catch (e) {
+            res.json({ r: 'err', err: e })
+        }
+    });
+
+    router.get('/delete/id/:id', async (req, res) => {
+        try {
+            const database = db_client.db('node_stduy');
+            const memo = database.collection('memo');
+            let _res = await memo.deleteOne({ _id: new ObjectId(req.params.id) });
+
+            res.json({ r: 'ok', d: _res })
+
+        }
+        catch (e) {
+            res.json({ r: 'err', d: e })
+        }
+
+    });
+    return router;
+}
+
--- day8/exam04_mongodb/index.js
+++ day8/exam04_mongodb/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { MongoClient } from 'mongodb'
+import { MongoClient} from 'mongodb'
+
+import memoDbRouter from './db_memo.js'
+
 dotenv.config();
+
 
 
 (async () => {
@@ -16,46 +20,13 @@ dotenv.config();
         //express 실행
         const app = express();
         app.use(express.json()); //json 미들웨어 등록 , 바디파서
+        app.use('/api/v1/memo/',memoDbRouter(db_client));
 
         if (process.env.NODE_ENV == 'dev') {
             app.get('/hello', (req, res) => {
                 res.json({ r: 'ok' })
             });
         }
-
-        app.post('/insert', async (req, res) => {
-
-            try {
-                const database = db_client.db('node_stduy');
-                const memo = database.collection('memo');
-
-                console.log(req.body)
-
-                let _res = await memo.insertOne(req.body);
-
-                res.json({ r: 'ok', d: _res });
-            }
-            catch (e) {
-                res.json({ r: 'err', err: e })
-            }
-        });
-
-        app.get('/find/skip/:skip/limit/:limit', async (req, res) => {
-            try {
-                console.log(req.params)
-                const database = db_client.db('node_stduy');
-                const memo = database.collection('memo');
-                let cursor = await memo.find()
-                    .skip(parseInt(req.params.skip))
-                    .limit(parseInt(req.params.limit));
-
-                let items = await cursor.toArray();
-                res.json({ r: 'ok', d: items });
-            }
-            catch (e) {
-                res.json({ r: 'err', err: e })
-            }
-        })
 
         app.listen(process.env.PORT, () => {
 

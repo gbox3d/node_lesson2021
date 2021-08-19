48eea32ec996dcc8bce213eb856394b8319c8acd
gbox3d <gbox3d@gmail.com>
2021-08-19 10:45:53 +0900

    ...

---
+++ day8/exam01/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="goHome"> 홈으로... </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name : "About",
+  methods : {
+    goHome() {
+      this.$router.push({
+        "path" : "/"
+      })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ day8/exam01/src/views/Help.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>도움말</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Help",
+  components: {},
+};
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>

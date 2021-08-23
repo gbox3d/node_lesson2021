473f6b3f5c2582fcd8940a7b1e0eef7177651098
gbox3d <gbox3d@gmail.com>
2021-08-23 10:53:30 +0900

    ...

--- day10/exam01_scss/src/App.js
+++ day10/exam01_scss/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
 
@@ -6,18 +8,31 @@ function App() {
     backgroundColor: 'rgb(255,0,0)'
   }
 
-  let btn_color = 'blue'
+  let [btn_color,btn_colorSet] = useState('blue')
 
+  function changeColor() {
+    console.log(btn_color)
+    // btn_color = 'pink'
+    btn_colorSet('pink')
+  }
 
   return (
     <div className="App">
+      <div className="myui nav">
+        <ul>
+          <li className="activate" > <a href="">item 1</a> </li>
+          <li> <a href="">item 2</a> </li>
+          <li> <a href="">item 3</a> </li>
+        </ul>
+
+      </div>
       <div>
         <button className="myui btn" >Normal</button>
         <button className="myui btn" style={red_btn}  >Cancel</button>
-        <button className="myui btn" style={ {
+        <button className="myui btn"  style={ {
           backgroundColor : btn_color,
           padding : "32px 32px"
-        } }  > Info </button>
+        } } onClick={changeColor} > Info </button>
       </div>
     </div>
   );

fb72b47b649258b0586c7e1dbf848d096bcd5517
gbox3d <gbox3d@gmail.com>
2021-08-23 15:52:03 +0900

    ....

--- day10/exam01_scss/src/App.js
+++ day10/exam01_scss/src/App.js
@@ -9,6 +9,7 @@ import Contact from './views/Contact';
 import Detail from './views/Detail';
 import Exam02 from './views/Exam02CondiRender';
 import Exam03 from './views/Exam03Axios';
+import Exam04 from './views/Exam04UseEffect';
 
 function App() {
 
@@ -22,7 +23,7 @@ function App() {
           <li> <NavLink to="/contact">Conact</NavLink> </li>
           <li> <NavLink to="/exam02">exam02</NavLink> </li>
           <li> <NavLink to="/exam03">exam03</NavLink> </li>
-
+          <li> <NavLink to="/exam04">exam04</NavLink> </li>
         </ul>
       </div>
 
@@ -31,6 +32,7 @@ function App() {
         <Route path="/about" component={About} ></Route>    
         <Route path="/exam02" component={Exam02}></Route>
         <Route path="/exam03" component={Exam03}></Route>
+        <Route path="/exam04" component={Exam04}></Route>
         <Route path="/:id" component={Detail} ></Route>
         <Route path="/" component={Home} ></Route>
       </Switch>
---
+++ day10/exam01_scss/src/views/Exam04UseEffect.js
@@ -0,0 +1,42 @@
+
+import React ,{useEffect,useState} from 'react'
+
+const Exam04 = ()=> {
+    const [count1, count1Set] = useState(0)
+    const [count2, count2Set] = useState(0)
+
+    useEffect(()=> {
+        console.log('update all')
+        
+        //on destory
+        return ()=> {
+            console.log('destroy all') 
+        }
+    })
+
+    useEffect(()=> {
+        console.log('counter 1 upadte')
+    },[count1])
+
+    useEffect(()=> {
+        console.log('counter 2 upadte')
+    },[count2])
+
+    useEffect(()=> {
+        console.log('counter 1 or 2 upadte')
+    },[count1,count2])
+
+    
+
+    return (
+        <div>
+            <h1>useEffect</h1>
+            <h3 onClick={()=>{count1Set(count1+1)}}> counter 1 : {count1}</h3>
+            <h3 onClick={()=>{count2Set(count2+1)}}> counter 2 : {count2}</h3>
+
+        </div>
+        
+    )
+}
+
+export default Exam04;
